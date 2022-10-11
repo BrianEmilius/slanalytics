f34091cdec97d39e1fc60a0fc345961bc22f6202
Brian Emilius <be@rts.dk>
2022-10-11 19:18:14 +0200

    move time calculation from sender to receiver

--- netlify/functions/create-record.js
+++ netlify/functions/create-record.js
@@ -4,7 +4,7 @@ async function createRecord(body) {
 	const db = await connect()
 	const bulk = db.collection("records").initializeUnorderedBulkOp()
 
-	body.records?.forEach(record => bulk.insert({ space: body.space, time: body.time, key: record }))
+	body.records?.forEach(record => bulk.insert({ space: body.space, time: new Date().valueOf(), key: record }))
 
 	const result = await bulk.execute()
 
